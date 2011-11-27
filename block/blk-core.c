2aa9d7146f0584f1b35ccc4c3d07a4913ba637ea
Michel Juillard <manveru0@gmail.com>
2011-11-27 05:06:39 +0100

    fix crash in scsi_dispatch_cmd()
    [ upstream commit bfe159a ]
    
    USB surprise removal of sr is triggering an oops in
    scsi_dispatch_command().  What seems to be happening is that USB is
    hanging on to a queue reference until the last close of the upper
    device, so the crash is caused by surprise remove of a mounted CD
    followed by attempted unmount.
    
    The problem is that USB doesn't issue its final commands as part of
    the SCSI teardown path, but on last close when the block queue is long
    gone.  The long term fix is probably to make sr do the teardown in the
    same way as sd (so remove all the lower bits on ejection, but keep the
    upper disk alive until last close of user space).  However, the
    current oops can be simply fixed by not allowing any commands to be
    sent to a dead queue.
    
    Cc: stable@kernel.org
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -890,6 +890,9 @@ static struct request *get_request_wait(struct request_queue *q, int rw_flags,
 struct request *blk_get_request(struct request_queue *q, int rw, gfp_t gfp_mask)
 {
 	struct request *rq;
+	
+	if (unlikely(test_bit(QUEUE_FLAG_DEAD, &q->queue_flags)))
+	  return NULL;
 
 	BUG_ON(rw != READ && rw != WRITE);
 

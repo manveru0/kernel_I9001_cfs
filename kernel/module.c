3bbea75431fa3ea17466147fe929609be809c2d9
Michel Juillard <manveru0@gmail.com>
2011-11-27 03:39:32 +0100

    allow changing the localversion [eidelen]

--- kernel/module.c
+++ kernel/module.c
@@ -2230,10 +2230,15 @@ static noinline struct module *load_module(void __user *umod,
 		if (err)
 			goto free_hdr;
 	} else if (!same_magic(modmagic, vermagic, versindex)) {
-		printk(KERN_ERR "%s: version magic '%s' should be '%s'\n",
-		       mod->name, modmagic, vermagic);
-		err = -ENOEXEC;
+	// we should try to load it anyway
+	err = try_to_force_load(mod, "vermagic doesn't equal");
+		if (err)
 		goto free_hdr;
+		/*
+		printk(KERN_ERR "%s: version magic '%s' should be '%s'\n",
+		mod->name, modmagic, vermagic);
+		err = -ENOEXEC;
+		goto free_hdr;*/
 	}
 
 	staging = get_modinfo(sechdrs, infoindex, "staging");
